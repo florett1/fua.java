cc9a11c7df741046c2eddf3753a25c13e6bedacc
florett1 <70637056+florett1@users.noreply.github.com>
2020-11-05 15:03:11 +0100

    fix final

--- src/fua.java
+++ src/fua.java
@@ -1,4 +1,16 @@
 public class fua {
     private final static String[] s = {"1"};
-    int x= 0;
+
+    public static void main (String[] args){
+        int erik = 100000;
+        int eimer = 0;
+        bast(erik, eimer);
+    }
+    public static void bast(int erik, int eimer) {
+    if(erik >eimer)
+
+    {
+        System.out.println("GGEZ");
+    }
+}
 }
